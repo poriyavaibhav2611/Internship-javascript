7f35e3c2aff28af4a359e81f5353e3011ef5bebb
poriyavaibhav2611 <poriyavaibhav2611@gmail.com>
2026-01-15 14:57:48 +0530

    Comment out code for checkEven function and API response simulation

--- Task 6 Promise/index.js
+++ Task 6 Promise/index.js
@@ -18,19 +18,63 @@
 	// Reject with "Odd number" if odd.
 
 
-let number = 3;
+// let number = 3;
 
-function checkEven(number){
+// function checkEven(number){
 
-    return new Promise((resolve,reject) => {
+//     return new Promise((resolve,reject) => {
 
-        if(number % 2 == 1){
-            resolve("Even Number");
-        }
-        else if(number % 2 == 0){
-            reject("Odd number")
-        }
-    })
-}
+//         if(number % 2 == 1){
+//             resolve("Even Number");
+//         }
+//         else if(number % 2 == 0){
+//             reject("Odd number")
+//         }
+//     })
+// }
 
-checkEven(number).then(msg => console.log(msg)).catch(err => console.log(err));
+// checkEven(number).then(msg => console.log(msg)).catch(err => console.log(err));
+
+// task 3 = Simulate a fake API call that takes 3 seconds and returns a list of users
+
+// let f_api = 
+// [
+//   {
+//     "id": 1,
+//     "name": "John Doe",
+//     "email": "john@example.com",
+//     "status": "active"
+//   },
+//   {
+//     "id": 2,
+//     "name": "Jane Smith",
+//     "email": "jane@example.com",
+//     "status": "inactive"
+//   },
+//   {
+//     "id": 3,
+//     "name": "Peter Jones",
+//     "email": "peter@example.com",
+//     "status": "active"
+//    }
+// ];
+
+// function apiResponse(f_api) {
+    
+//     return new Promise((resolve, reject)=>{
+
+//         let sucess = true; // true or false to test both resolve and reject
+//         if(sucess){
+//             setTimeout(() => {
+//                 resolve(f_api);
+//             }, 3000);
+//         }
+//         else{
+//             reject("Error: Unable to fetch data"); 
+//         }
+//     })
+// }
+
+// apiResponse(f_api)
+// .then(getdata => console.log(getdata))
+// .catch(err => console.log(err));
