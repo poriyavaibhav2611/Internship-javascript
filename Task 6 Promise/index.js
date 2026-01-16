779126084b83a8a23e320fc3ab72ffa3e12d9294
poriyavaibhav2611 <poriyavaibhav2611@gmail.com>
2026-01-16 12:45:54 +0530

    Refactor checkEven function logic and add task functions for sequential promise execution

--- Task 6 Promise/index.js
+++ Task 6 Promise/index.js
@@ -18,16 +18,16 @@
 	// Reject with "Odd number" if odd.
 
 
-// let number = 3;
+// let number = 2;
 
 // function checkEven(number){
 
 //     return new Promise((resolve,reject) => {
 
-//         if(number % 2 == 1){
+//         if(number % 2 == 0){
 //             resolve("Even Number");
 //         }
-//         else if(number % 2 == 0){
+//         else if(number % 2 == 1){
 //             reject("Odd number")
 //         }
 //     })
@@ -77,4 +77,95 @@
 
 // apiResponse(f_api)
 // .then(getdata => console.log(getdata))
+// .catch(err => console.log(err));
+
+// task 4 = Create 3 functions:
+
+// 	startTask() → resolves "Task started"
+
+// 	processTask() → resolves "Task in progress"
+
+// 	endTask() → resolves "Task completed"
+	
+// 	Call them in sequence using .then() chaining.
+
+// function startTask() {
+	
+// 	return new Promise((resolve) => {
+// 		setTimeout(()=>{
+// 			resolve("Task Started");
+// 		})
+// 	});
+// }
+
+// function processTask() {
+	
+// 	return new Promise((resolve) => {
+// 		setTimeout(()=>{
+// 			resolve("Task in progress");
+// 		},2000)
+// 	});
+// }
+
+// function endTask() {
+	
+// 	return new Promise((resolve) => {
+// 		setTimeout(()=>{
+// 			resolve("Task completed");
+// 		},3000)
+// 	});
+// }
+
+// startTask().then(start => console.log(start));
+// processTask().then(process => console.log(process));
+// endTask().then(end => console.log(end));
+
+// task 5=Step 1: User places an order — takes 1 second.
+
+	// Step 2: Check if the item is in stock — takes 2 seconds.
+
+	// 	If item is "pizza", it's available.
+
+	// 	For any other item, reject with "Item not available".
+
+	// Step 3: Process payment — takes 1.5 seconds.
+
+	// Step 4: Confirm order.
+
+
+// function pizzAvailable() {
+	
+// 	return new Promise((resolve,reject) => {
+// 		let pizza = true;
+
+// 		setTimeout(() => {
+// 			console.log("User placed an order");
+// 		},1000);
+
+// 		setTimeout(() => {
+// 			if(pizza == true){
+// 				resolve("Pizza is available");
+// 			}
+// 			else{
+// 				reject("Item not available");
+// 			}
+// 		},2000);
+
+// 		setTimeout(() => {
+// 			if(pizza == false){
+// 				return;
+// 			}
+// 			console.log("Process payment")
+// 		},3500);
+
+// 		setTimeout(() => {
+// 			if(pizza == false){
+// 				return;
+// 			}
+// 			console.log("Confirm order")
+// 		},5000)
+// 	})
+// }
+// pizzAvailable()
+// .then(msg => console.log(msg))
 // .catch(err => console.log(err));
